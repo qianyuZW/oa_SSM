f068d0ae525ded2b0b665453b42d33c89decff28
liuzhou <497438584@qq.com>
2018-12-08 21:07:20 +0800

    修改启动url

--- src/main/java/org/ppcirgo/oa/OaApplication.java
+++ src/main/java/org/ppcirgo/oa/OaApplication.java
@@ -3,11 +3,13 @@ package org.ppcirgo.oa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@MapperScan(basePackages = {"org.ppcirgo.oa.mapper"})
+
 @SpringBootApplication
 @EnableScheduling
+@MapperScan(basePackages = {"org.ppcirgo.oa.mapper"})
 public class OaApplication {
     public static void main(String[] args) {
         SpringApplication.run(OaApplication.class, args);
