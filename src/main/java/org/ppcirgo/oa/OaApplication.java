e7b428624a094ab8e206e830f226589b80979aad
liuzhou <497438584@qq.com>
2018-12-08 19:02:08 +0800

    加上心跳

--- src/main/java/org/ppcirgo/oa/OaApplication.java
+++ src/main/java/org/ppcirgo/oa/OaApplication.java
@@ -3,9 +3,11 @@ package org.ppcirgo.oa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan(basePackages = {"org.ppcirgo.oa.mapper"})
 @SpringBootApplication
+@EnableScheduling
 public class OaApplication {
     public static void main(String[] args) {
         SpringApplication.run(OaApplication.class, args);
