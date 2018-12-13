5c1c645710be9866863e4803c1663ccbe0ac3b1c
minminminzhang <minzhang1534781927@163.com>
2018-12-13 15:49:49 +0800

    sendMail code

--- src/main/java/org/ppcirgo/oa/beans/model/UserModel.java
+++ src/main/java/org/ppcirgo/oa/beans/model/UserModel.java
@@ -14,4 +14,52 @@ public class UserModel implements Serializable {
     private String password;
     private String level;//用户等级，用作简单权限控制
     private String email;//注册邮箱
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
