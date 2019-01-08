6aacd3e1648a9a36f87712ec48b69caf15aa5cb4
寮犺枃 <18326629522@163.com>
2019-01-08 14:58:04 +0800

    鏆傛椂鎻愪氦

--- src/main/java/org/ppcirgo/oa/mapper/PlanMapper.java
+++ src/main/java/org/ppcirgo/oa/mapper/PlanMapper.java
@@ -11,14 +11,14 @@ import javax.xml.soap.Text;
 @Repository
 public interface PlanMapper {
     //添加周报
-    @Insert(value = "insert into weekPlan(user_id,create_time,visited_store,create_day,comment,shipping_plan,development_store) values(#{userId},#{createTime},#{visitedStore},#{createDay},#{comment},#{shippingPlan},#{developmentStore})")
+    @Insert(value = "insert into week_plan(user_id,create_time,visited_store,create_day,comment,shipping_plan,development_store) values(#{userId},#{createTime},#{visitedStore},#{createDay},#{comment},#{shippingPlan},#{developmentStore})")
     int savePlan(@Param("userId") int userId,@Param("createTime") long createTime, @Param("visitedStore") String visitedStore, @Param("createDay") long createDay, @Param("comment") String comment, @Param("shippingPlan") String shippingPlan,@Param("developmentStore") String developmentStore);
     //根据用户id查周报
-    @Select(value = "select * from weekPlan where user_id = #{userId}")
+    @Select(value = "select * from week_plan where user_id = #{userId}")
     PlanModel getPlanByUserId(@Param("userId") int userId);
     //修改周计划
-    @Update(value =  "update weekPlan set modify_time = #{modifyTime}, modify_day=#{modifyDay}, visited_store=#{visitedStore},comment=#{comment},shipping_plan=#{shippingPlan},development_store=#{developmentStore} where user_id = #{userId}")
+    @Update(value =  "update week_plan set modify_time = #{modifyTime}, modify_day=#{modifyDay}, visited_store=#{visitedStore},comment=#{comment},shipping_plan=#{shippingPlan},development_store=#{developmentStore} where user_id = #{userId}")
     int modifyPlan(@Param("userId") int userId, @Param("modifyTime") long modifyTime, @Param("modifyDay") long modifyDay, @Param("visitedStore") String visitedStore,@Param("comment") String comment,@Param("shippingPlan") String shippingPlan,@Param("developmentStore") String developmentStore);
-    @Delete(value="delete from weekPlan where user_id = #{userId}")
+    @Delete(value="delete from week_plan where user_id = #{userId}")
     int deletePlanById(@Param("userId") int userId);
 }
