3691b9be992508ddfc289c406c5ebd98d9f50191
minminminzhang <minzhang1534781927@163.com>
2019-01-08 15:30:00 +0800

    .

--- src/main/java/org/ppcirgo/oa/mapper/AttendanceMapper.java
+++ src/main/java/org/ppcirgo/oa/mapper/AttendanceMapper.java
@@ -13,34 +13,34 @@ import org.springframework.stereotype.Repository;
 public interface AttendanceMapper {
 
     //根据attendance_name获取签到时间start_time
-    @Select(value="select start_time  from  attendance1 where id=#{id}")
+    @Select(value="select start_time  from  attendance where id=#{id}")
     String  getStartTimeById(@Param("id") Integer id );
 
 
     //根据attendance_name获取签到时间end_time
-    @Select(value="select end_time from  attendance1 WHERE id=#{id}")
+    @Select(value="select end_time from  attendance WHERE id=#{id}")
     String getEndTimeById(@Param("id") Integer id);
 
     //保存打卡记录
-    @Insert(value="insert into attendance1(employee_id,attendance_name,start_time,end_time,DATE,STATUS,start_location,start_longitude,start_latitude,end_location,end_longitude,end_latitude,\n" +
+    @Insert(value="insert into attendance(employee_id,attendance_name,start_time,end_time,DATE,STATUS,start_location,start_longitude,start_latitude,end_location,end_longitude,end_latitude,\n" +
             "start_mile,start_img,end_mile,end_img) values(#{employee_id},#{attendance_name},#{start_time},#{end_time},#{date},#{status},#{start_location},#{start_longitude},#{start_latitude},\n" +
             " #{end_location},#{end_longitude},#{end_latitude},#{start_mile},#{start_img},#{end_mile},#{end_img})")
     int saveAttendanceRecord(AttendanceModel attendanceModel);
 
    //根据id更新打卡的结束时间和状态
-    @Update(value="update attendance1 set end_time=#{end_time} where id=#{id}")
+    @Update(value="update attendance set end_time=#{end_time} where id=#{id}")
     int updateEndTimeAndStatus(@Param("end_time") String end_time,@Param("id") Integer id);
 
     //跟根据uid更新打卡的签到时间和状态
-    @Update(value="update attendance1 set start_time=#{start_time} where id=#{id}")
+    @Update(value="update attendance set start_time=#{start_time} where id=#{id}")
     int updateStartTimeAndStatus(@Param("start_time") String start_time,@Param("id") Integer id);
 
 
     //根据员工id获得每月的出勤率
-    @Select(value="select sum(status=\"打卡签退\") from attendance1 where employee_id=#{employee_id}")
+    @Select(value="select sum(status=\"打卡签退\") from attendance where employee_id=#{employee_id}")
     String getWorkTimesByEmployeeId(@Param("employee_id") Integer employee_id);
 
     //查询员工在某个时间段内的里程数
-    @Select(value="select sum(end_mile-start_mile) as miles from attendance1 where employee_id=#{employee_id} and (date between #{date1} and #{date2})")
+    @Select(value="select sum(end_mile-start_mile) as miles from attendance where employee_id=#{employee_id} and (date between #{date1} and #{date2})")
     String getSumsOfMiles(@Param("employee_id") Integer employee_id, @Param("date1") String date1, @Param("date2") String date2);
 }
